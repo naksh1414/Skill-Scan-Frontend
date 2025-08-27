51f8ecba5a21d0458ec25a3513ce0bbed254dd57
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2025-08-27 22:27:04 +0530

    Host Config Changed

--- src/apis/ApiRoutes.js
+++ src/apis/ApiRoutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://Skill Scan.onrender.com/api";
-export const host = "http://localhost:4000/api";
+export const host = "https://skill-scan-backend-iwn6.onrender.com/api";
+// export const host = "http://localhost:4000/api";
 export const auth = `${host}/auth`;
 export const user = `${host}/user`;
 export const job = `${host}/jobs`;
